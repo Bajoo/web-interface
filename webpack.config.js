b40f03b475250e284cf9bc96c2c908d120bef39c
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2017-04-24 23:37:26 +0200

    Improves webpack config; Add a prod build configuration.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+
+module.exports = {
+    devtool: "source-map",
+    entry: "./src/index.js",
+    output: {
+        sourceMapFilename: "bundle.js.map",
+        pathinfo: (process.env.NODE_ENV !== 'production'),
+        path: __dirname + '/bin/', //Path where the bundle should be exported
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['es2015']
+            },
+            exclude: [/node_modules/]
+        }]
+    }
+};
