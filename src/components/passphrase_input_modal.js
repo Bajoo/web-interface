3be7c0531c759c441aa7bdc78f42698426f1a235
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2017-06-19 12:54:24 +0200

    Refactor passphrase input modal

--- src/components/passphrase_input_modal.js
+++ src/components/passphrase_input_modal.js
@@ -2,6 +2,7 @@
 import m from 'mithril';
 import {_} from '../utils/i18n';
 import InputForm from './input_form';
+import Modal from './modal';
 
 
 export default class PassphraseInputModal {
@@ -18,37 +19,26 @@ export default class PassphraseInputModal {
      * @param model {PassphraseInput}
      */
     view({attrs: {model}}) {
-        return m('.modal[role=dialog][aria-labelledby=passphrase-input-modal]',
-            { class: model.enabled ? 'show' : ''},
-            m('.modal-dialog[role=document]', m('.modal-content', [
-                m('.modal-header', [
-                    m('button.close[aria-label=Close][type=button]', {onclick: () => model.cancel()},
-                        m('span[aria-hidden=true]', m.trust('&times;'))
-                    ),
-                    m('h4.modal-title#passphrase-input-modal', _('Your passphrase is required'))
-                ]),
-                m('.modal-body', [
-                    _('Please, enter your passphrase to encrypt or decrypt your files.'), m('br'), m('br'),
+        return Modal.make(v => typeof v === 'undefined' ? model.enabled : model.enabled = v,
+            _('Your passphrase is required'), 'passphrase-input-modal', [
+                _('Please, enter your passphrase to encrypt or decrypt your files.'), m('br'), m('br'),
 
-                    m('form#passphrase-input-form', { onsubmit: () => {model.submit(); return false;} }, [
-                        model.error ?
-                            m('.alert .alert-danger', _("We're unable to decrypt the key. Bad passphrase ?")) :
-                            '',
-                        m('fieldset', { disabled: model.wait_for_feedback}, [
-                            m(InputForm, {id: 'passphrase', label: _('Passphrase'), icon: 'lock', type: 'password',
-                                value: model.passphrase, autofocus: true})
-                        ])
+                m('form#passphrase-input-form', { onsubmit: () => {model.submit(); return false;} }, [
+                    model.error ?
+                        m('.alert .alert-danger', _("We're unable to decrypt the key. Bad passphrase ?")) :
+                        '',
+                    m('fieldset', { disabled: model.wait_for_feedback}, [
+                        m(InputForm, {id: 'passphrase', label: _('Passphrase'), icon: 'lock', type: 'password',
+                            value: model.passphrase, autofocus: true})
                     ])
-                ]),
-                m('.modal-footer', [
-                    m('button.btn.btn-default[type=button]', {
-                        disabled: model.wait_for_feedback,
-                        onclick: () => model.cancel()
-                    }, _('Cancel')),
-                    m('button.btn.btn-primary[type=submit][form=passphrase-input-form]',
-                        {disabled: model.wait_for_feedback}, _('Apply'))
                 ])
-            ]))
-        );
+            ], [
+                m('button.btn.btn-default[type=button][form=passphrase-input-form]', {
+                    disabled: model.wait_for_feedback,
+                    onclick: () => model.cancel()
+                }, _('Cancel')),
+                m('button.btn.btn-primary[type=submit][form=passphrase-input-form]',
+                    {disabled: model.wait_for_feedback}, _('Apply'))
+            ]);
     }
 }
