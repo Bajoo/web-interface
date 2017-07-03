762c00d88e2a698b442da85d948e0cf5029cc815
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2017-07-03 17:28:15 +0200

    Rewrite webpack config
    CSS files are now in a distinct folder. CSS '@import' are put inline by webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,115 @@
 
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-let all_in_one = false;
-
-let file_loader = all_in_one ? 'url-loader' : 'file-loader';
+const extract_html = new ExtractTextPlugin('index.html');
+const extract_css = new ExtractTextPlugin('style.css');
 
 
-module.exports = {
-    devtool: "source-map",
-    entry: {
-        app: ['babel-polyfill', './src/index.js'],
-    },
-    output: {
-        sourceMapFilename: "js/[name].js.map",
-        pathinfo: (process.env.NODE_ENV !== 'production'),
-        path: path.join(__dirname, 'dist'),
-        filename: "js/[name].js"
-    },
-    externals: {
-        mithril: 'm',
-        openpgp: 'openpgp'
-    },
-    module: {
-        loaders: [{
-            test: /\.html$/,
-            loader: 'html-loader?attrs[]=link:href&attrs[]=script:src'
-        }, {
-            test: /\.css$/,
-            loaders: [
-                "file-loader?name=css/[name].[ext]",
-                "extract-loader?publicPath=../",
-                'css-loader'
-            ]
-        }, {
-            test: /\.(woff|woff2|eot|ttf|svg)$/,
-            loader: `${file_loader}?name=fonts/[name].[ext]`
-        }, {
-            test: path.join(__dirname, 'static/favicon.ico'),
-            loader: `${file_loader}?name=favicon.ico`
-        }, {
-            test: /static\/favicon\//,
-            loader: `${file_loader}?name=favicon/[name].[ext]`,
-        }, {
-            test: /\.(png)$/,
-            exclude: /static\/favicon\//,
-            loader: `${file_loader}?name=images/[name].[ext]`,
-        }, {
-            test: /(mithril|openpgp(\.worker)?)(\.min)?\.js$/,
-            loader: 'file-loader?name=js/[name].[ext]'
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            inlineSource: all_in_one ? '.js$' : null
-        }),
-        new HtmlWebpackInlineSourcePlugin()
-    ]
-};
+const common_loaders = [
+    {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+    }, {
+        test: path.join(__dirname, 'static/favicon.ico'),
+        loader: 'file-loader?name=favicon.ico'
+    }, {
+        test: /static\/favicon\//,
+        loader: 'file-loader?name=favicon/[name].[ext]',
+    }, {
+        test: /\.(png)$/,
+        exclude: /static\/favicon\//,
+        loader: 'file-loader?name=images/[name].[ext]',
+    }, { // Copy CSS vendor dependencies, and extract related dependencies (fonts, images)
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: [
+            "file-loader?name=[ext]/[name].[ext]",
+            "extract-loader?publicPath=../",
+            'css-loader'
+        ],
+    }, {
+        test: /\.js$/,
+        include: [/node_modules\/openpgp/, /node_modules\/mithril/],
+        loader: 'file-loader?name=[ext]/[name].[ext]',
+    }
+];
+
+
+module.exports = [
+    { // HTML build config
+        entry: './index.html',
+        output: {
+            pathinfo: (process.env.NODE_ENV !== 'production'),
+            path: path.join(__dirname, 'dist'),
+            filename: 'index.html'
+        },
+        module: {
+            loaders: [{
+                test: /\.html$/,
+                loader: extract_html.extract({
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: [
+                                'link:href',
+                                'script:src'
+                            ]
+                        }
+                    }
+                })
+            }, { // Update source path to match location of other built chunks.
+                test: /\.(js|css)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    emitFile: false,
+                    name: './[ext]/[name].[ext]'
+                }
+            }
+            ].concat(common_loaders)
+        },
+        plugins: [extract_html]
+    }, {
+        // CSS build config
+        entry: './css/style.css',
+        output: {
+            pathinfo: (process.env.NODE_ENV !== 'production'),
+            path: path.join(__dirname, 'dist/css'),
+            filename: 'style.css'
+        },
+        module: {
+            loaders: [{
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: extract_css.extract({
+                    use: 'css-loader'
+                })
+            }]
+        },
+        plugins: [extract_css]
+    }, {
+        // JS build config
+        devtool: "source-map",
+        entry: {
+            index: ['babel-polyfill', './src/index.js'],
+        },
+        output: {
+            sourceMapFilename: "js/[name].js.map",
+            pathinfo: (process.env.NODE_ENV !== 'production'),
+            path: path.join(__dirname, 'dist'),
+            filename: "js/[name].js"
+        },
+        externals: {
+            mithril: 'm',
+            openpgp: 'openpgp'
+        },
+        module: {
+            loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }].concat(common_loaders)
+        }
+    }
+];
