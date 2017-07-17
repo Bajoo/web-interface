d458533790e26995d266b77bff7724c042ff2964
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2017-07-17 11:59:51 +0200

    Add utility class AsyncProp

---
+++ src/utils/async_prop.js
@@ -0,0 +1,135 @@
+
+
+/**
+ * Wrapper of asynchronously-loaded property
+ *
+ * It handles all elements related to the loading of an arbitrary value:
+ * - the different states (loading, error, loaded)
+ * - concurrent access to load the values
+ *
+ * Warning: the `instanceof` operator doesn't work with AsyncProp instances.
+ *
+ * @template T
+ */
+export default class AsyncProp {
+    constructor() {
+        /*
+         * `this.ref` is the object returned by the constructor. It means all properties acceded for the instance are
+         * stored in `this.ref`.
+         * Also, all methods except the constructor and `_call()` will reference `this.ref` as `this`.
+         */
+        this.ref = this._call.bind(this);
+        this.ref.constructor = this.constructor;
+        this.ref.prototype = this.prototype;
+        Object.setPrototypeOf(this.ref, this.constructor.prototype);
+
+        /**
+         * Value wrapped, if set. `undefined` means there is no value.
+         * @type {undefined|T}
+         */
+        this.ref.value = undefined;
+
+        /** @type {?Error} */
+        this.ref.error = null;
+
+        /** @type {?Promise} */
+        this.ref.promise = null;
+
+        /**
+         * Callback executed when an error occurs (during loading)
+         * @type {?function(error: Error)}
+         */
+        this.ref.onerror = null;
+
+        return this.ref;
+    }
+
+    /**
+     * Getter/Setter of the wrapped value
+     *
+     * It's called by a direct call of the AsyncProp instance
+     *
+     * @param {T} [new_value] - if set, replace the value.
+     * @return {undefined|T} - the last value set (or undefined)
+     *
+     * @example
+     * let prop = new AsyncProp();
+     * prop(33); // setter
+     * let value = prop(); // getter
+     */
+    _call(new_value = undefined) {
+        let that = this.ref;
+        if (new_value !== undefined) {
+            that.value = new_value;
+            that.error = null;
+        }
+        return that.value;
+    }
+
+    /**
+     * Load (or reload) the data.
+     *
+     * When called, the loader is called. The AsyncProp is set in 'loading' state until the promise resolution.
+     * At this point, the AsyncProp use the promise result to set its value or its error.
+     *
+     * Note that if load() is called when the prop is already in loading, the new loader is ignored.
+     *
+     * @param {function(): Promise<T>} loader
+     * @return {Promise<AsyncProp<T>>} new Promise resolving with this AsyncProp (after values being set).
+     *   The promise is always successful: errors are captured in the AsyncProp instance.
+     */
+    load(loader) {
+        if (this.promise) // reuse ongoing promise
+            return this.promise;
+
+        this.promise = loader().then(res => {
+            this.promise = null;
+            this(res);
+            return this;
+        }, err => {
+            this.promise = null;
+            this.set_error(err);
+
+            // TODO: wrap callbacks for catching unexpected exceptions
+            if (this.onerror)
+                this.onerror(err);
+
+            return this;
+        });
+        return this.promise;
+    }
+
+    /**
+     * Set the property in error state (and erase the value).
+     *
+     * Warning: it does NOT trigger the `onerror` callback.
+     *
+     * @param {Error} err - new error
+     */
+    set_error(err) {
+        this.error = err;
+        this.value = null;
+    }
+
+    /**
+     * Call one of the three callbacks, depending on the status of the property.
+     *
+     * All callbacks are optionals. If a non-defined (or `null`) callback should be called, the `null` value is
+     * returned instead.
+     *
+     * The loading callback is only called during the initial loading. On subsequent loadings, the "loaded" callback is
+     * used (as there is the previous value).
+     *
+     * @template U
+     * @param {?function(value: T): U} [loaded] - Called if the property is loaded
+     * @param {?function(): U} [loading] - Called if the property is not yet loaded
+     * @param {?function(error: Error): U} [failed] - Called if the loading has failed
+     */
+    dispatch(loaded, loading, failed) {
+        if (this.error)
+            return failed ? failed(this.error) : null;
+        if (this.value === undefined)
+            return loading ? loading() : null;
+        return loaded ? loaded(this.value) : null;
+    }
+}
---
+++ test/utils/async_prop.js
@@ -0,0 +1,218 @@
+import assert from "assert";
+import AsyncProp from "../../src/utils/async_prop";
+
+
+/** @test {AsyncProp} */
+describe('AsyncProp', () => {
+
+    it('can be instanced', () => {
+        let prop = new AsyncProp();
+    });
+
+    it('is not set at instantiation', () => {
+        let prop = new AsyncProp();
+        assert.deepEqual(prop(), undefined);
+    });
+
+    it('has no error at instantiation', () => {
+        let prop = new AsyncProp();
+        assert.deepEqual(prop.error, null);
+    });
+
+    // 1. can be called to set the result
+    // can be called to GET the result.
+
+    it('can get and set values by function call', () => {
+        let prop = new AsyncProp();
+        prop(33);
+        assert.equal(prop(), 33);
+        let value = {};
+        prop(value);
+        assert.deepEqual(prop(), value);
+    });
+
+    it('removes any error when a new value is set', () => {
+        let prop = new AsyncProp();
+        prop.error = new Error('!!');
+        prop('new value');
+        assert.deepEqual(prop.error, null);
+    });
+
+    it('keep the error when the value is read', () => {
+        let prop = new AsyncProp();
+        let err = new Error('!!');
+        prop.error = err;
+        prop();
+        assert.deepEqual(prop.error, err);
+    });
+
+    /** @test {AsyncProp#set_error} */
+    describe('#set_error()', () => {
+
+        /** @test {AsyncProp#set_error} */
+        it('remove the error attribute', () =>  {
+            let prop = new AsyncProp();
+            let err = new Error('!!');
+            prop.set_error(err);
+            assert.deepEqual(prop.error, err);
+        });
+
+        /** @test {AsyncProp#set_error} */
+        it('remove the stored value', () =>  {
+            let prop = new AsyncProp();
+            prop.value = 'exists';
+            prop.set_error(new Error('!!'));
+            assert.deepEqual(prop.value, undefined);
+        });
+    });
+
+    describe('#load()', () => {
+
+        it('should call the loader', () => {
+            let prop = new AsyncProp();
+            let success = false;
+            let loader = () => {
+                success = true;
+                return Promise.resolve();
+            };
+
+            return prop.load(loader).then(() => {
+                assert.deepEqual(success, true);
+            });
+        });
+
+        it('should return a promise wrapping the AsyncProp instance', () => {
+            let prop = new AsyncProp();
+
+            return prop.load(() => Promise.resolve(true)).then(that => {
+                assert.deepEqual(that, prop);
+            });
+        });
+
+        it('should set the value when the loader promise is fulfilled', () => {
+            let prop = new AsyncProp();
+            let loader = () => Promise.resolve(753);
+
+            return prop.load(loader).then(() => {
+                assert.deepEqual(prop.value, 753);
+            });
+        });
+
+        it('should return a promise always fulfilled', () => {
+            let prop = new AsyncProp();
+            let loader = () => Promise.reject(new Error('Example error'));
+
+            return prop.load(loader);
+        });
+
+        it('should set the error when the loader promise is rejected', () => {
+            let prop = new AsyncProp();
+            let error = new Error('Example error');
+            let loader = () => Promise.reject(error);
+
+            return prop.load(loader).then(() => {
+                assert.deepEqual(prop.error, error);
+            });
+
+        });
+
+        it('should call the "onerror" callback when the promise is rejected', () => {
+            let prop = new AsyncProp();
+            let loader = () => Promise.reject(new Error('Example error'));
+
+            let success = false;
+            prop.onerror = () => success = true;
+
+            return prop.load(loader).then(() => {
+                assert.deepEqual(success, true);
+            });
+        });
+
+        it('should reuse the same promise when called twice or more at the same time', () => {
+            let prop = new AsyncProp();
+            let loader_call = 0;
+            let resolve_loader  = null;
+
+            function loader() {
+                console.log('CALL');
+                loader_call++;
+                return new Promise(resolve => resolve_loader = resolve);
+            }
+
+            let promise = prop.load(loader); // loader call ++
+            let p2 = prop.load(loader);
+            assert.deepEqual(promise, p2, 'X');
+            assert.deepEqual(promise, prop.load(loader)); // loader not called
+
+            promise.then(() => {
+                assert.notEqual(promise, prop.load(loader)); // loader call ++
+                assert.deepEqual(loader_call, 2);
+            });
+            resolve_loader();
+        });
+    });
+
+    describe('#dispatch()', () => {
+
+        function fail_test_cb() {
+            throw new Error('This callback should not be called');
+        }
+
+        it('should call the "loaded" callback if loaded', () => {
+            let prop = new AsyncProp();
+            prop.value = 999;
+
+            let res = prop.dispatch(value => {
+                assert.deepEqual(value, 999);
+                return 1000;
+            }, fail_test_cb, fail_test_cb);
+            assert.deepEqual(res, 1000);
+        });
+
+        it('should call the "loading" callback if not loaded', () => {
+            let prop = new AsyncProp();
+
+            let res = prop.dispatch(fail_test_cb, () => 'loading value', fail_test_cb);
+            assert.deepEqual(res, 'loading value');
+        });
+
+        it('should call the "failed" callback if in error', () => {
+            let prop = new AsyncProp();
+            prop.error = new Error('Example error');
+
+            let res = prop.dispatch(fail_test_cb, fail_test_cb, err => {
+                assert.deepEqual(err, prop.error);
+                return 'FAILBACK';
+            });
+            assert.deepEqual(res, 'FAILBACK');
+        });
+
+        it('should accept undefined arguments', () => {
+            let prop = new AsyncProp();
+
+            prop.value = 'Is loaded';
+            assert.equal(prop.dispatch(() => 'OK'), 'OK');
+        });
+
+        it('should return null if there is not callback', () => {
+            let prop = new AsyncProp();
+            assert.deepEqual(prop.dispatch(), null);
+
+            let loaded_prop = new AsyncProp();
+            loaded_prop.value = 'Defined';
+            assert.deepEqual(loaded_prop.dispatch(), null);
+        });
+
+        it('should return null if the callback argument is set to null', () => {
+            let prop = new AsyncProp();
+
+            let res = prop.dispatch(fail_test_cb, null, fail_test_cb);
+            assert.deepEqual(res, null);
+
+            let loaded_prop = new AsyncProp();
+            loaded_prop.value = 'Defined';
+            res = loaded_prop.dispatch(null, fail_test_cb, fail_test_cb);
+            assert.deepEqual(res, null);
+        });
+    });
+});
